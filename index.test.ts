27ab2e825b434e9545e970a40d970f417174a149
Aaron Sanchez <saarong12@gmail.com>
2024-06-12 23:58:10 -0500

    Added test to validate the convert boolean to string function

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,14 @@
 import * as funcs from './functions'
-import { describe, test, expect} from '@jest/globals'
+import { describe, test, expect } from '@jest/globals'
 
 describe('Abbreviate a two word name:', () => {
-  test('abbreviate `chravis bobussy`', ()=> {
+  test('`chravis bobussy` ==> `C.B`', () => {
     expect(funcs.AbbreviateATwoWordName('chravis bobussy')).toBe('C.B')
+  })
+})
+
+describe('Convert boolean to `yes` or `no`', () => {
+  test('`True` ==> `Yes`', () => {
+    expect(funcs.ConvertBoolean(true)).toBe('Yes')
   })
 })
