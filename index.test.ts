06c77d689e95516167a823ccef5e9b31ea1c8c1f
Aaron Sanchez <saarong12@gmail.com>
2024-06-13 23:11:53 -0500

    Added table driven tests for the functions already moved into the test file.

--- index.test.ts
+++ index.test.ts
@@ -1,14 +1,54 @@
 import * as funcs from './functions'
 import { describe, test, expect } from '@jest/globals'
 
-describe('Abbreviate a two word name:', () => {
-  test('`chravis bobussy` ==> `C.B`', () => {
-    expect(funcs.AbbreviateATwoWordName('chravis bobussy')).toBe('C.B')
+// 05/27/24
+describe('Abbreviate a two word name function:', () => {
+  type testCase = {
+    name: string
+    given: string
+    want: string
+  }
+
+  const testCases: testCase[] = [
+    {
+      name: 'Chravis Bobussy => C.B',
+      given: 'chravis bobussy',
+      want: 'C.B'
+    },
+    {
+      name: 'Jane Doe => J.D',
+      given: 'Jane Doe',
+      want: 'J.D'
+    }
+  ]
+
+  test.each(testCases)('$name', ({given, want}) => {
+    expect(funcs.AbbreviateATwoWordName(given)).toBe(want)
   })
 })
 
-describe('Convert boolean to `yes` or `no`', () => {
-  test('`True` ==> `Yes`', () => {
-    expect(funcs.ConvertBoolean(true)).toBe('Yes')
+// 05/28/24
+describe('Convert boolean function:', () => {
+  type testCase = {
+    name: string
+    given: boolean
+    want: string
+  }
+
+  const testCases: testCase[] = [
+    {
+      name: 'true => Yes',
+      given: true,
+      want: 'Yes'
+    },
+    {
+      name: 'false => No',
+      given: false,
+      want: 'No'
+    }
+  ]
+
+  test.each(testCases)('$name', ({given, want}) => {
+    expect(funcs.ConvertBoolean(given)).toBe(want)
   })
 })
--- index.ts
+++ index.ts
@@ -3,12 +3,6 @@
 
 import * as funcs from './functions/'
 
-// TODO Add back the other functions from the func dir.
-
-// console.log(funcs.AbbreviateATwoWordName('chravis bobussy')) // Should return C.B.
-
-// 05/28/24
-// console.log(funcs.ConvertBoolean(true)) // Should return 'Yes'.
 
 // 05/31/24
 // console.log(funcs.WhoLikesIt(['Peter', 'Jacob'])) // Should return "Peter and Jacob like this"
